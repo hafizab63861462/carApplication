0e46aa4300d324b08698728dcb2460c77ab65714
Hafiz Abdullah <hafiz.abdullahoffical@gmail.com>
2024-07-06 18:50:36 +0500

    login complete

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,10 @@
+'use client'
+
+const Dashboard = () => {
+
+  return (
+    <h1>Hello from car</h1>
+  );
+};
+
+export default Dashboard;
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -3,15 +3,25 @@
 import { useForm } from 'react-hook-form';
 import CommonForm from "@/app/components/commonForm"
 import { loginFormSchema } from "@/app/components/FormSchema/loginFoormSchema"
-
+import axios from 'axios';
+import { useRouter } from 'next/navigation'
 
 const LoginPage = () => {
   const { control, getValues } = useForm();
+  const router = useRouter()
 
   const onSubmit = async () => {
     try {
-      console.log('data', getValues());
-      // ... rest of your onSubmit logic
+      const apiUrl = process.env.CAR_APPLICATION_LOCAL_API_URL;
+
+      console.log('apiUrl', apiUrl);
+      const response = await axios.post(`http://localhost:5000/api/login`, getValues());
+      // const response = await axios.post(`${apiUrl}/login`, getValues());
+
+      const token = response.data.token;
+      localStorage.setItem('token', token);
+      router.push('/dashboard')
+
     } catch (error) {
       console.error(error);
     }
