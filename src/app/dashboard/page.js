084415a71e7e61c0b5e2b031b0da880f9c249943
Hafiz Abdullah <hafiz.abdullahoffical@gmail.com>
2024-07-07 20:09:28 +0500

    reset added

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 import { message } from 'antd';
 
 const Dashboard = () => {
-  const { control, getValues, setValue, watch, formState } = useForm({
+  const { control, getValues, setValue, watch, formState, reset } = useForm({
     mode: "onChange",
     resolver: yupResolver(validationSchema)
   });
@@ -19,7 +19,33 @@ const Dashboard = () => {
   const onSubmit = async () => {
     try {
       const apiUrl = process.env.NEXT_PUBLIC_CAR_APPLICATION_LOCAL_API_URL;
-      const response = await axios.post(`${apiUrl}/addCar`, getValues());
+
+      const formData = new FormData();
+      const values = getValues();
+
+      // Append form fields to FormData
+      for (const key in values) {
+        if (key !== 'uploadimage') { // Skip 'uploadimage' for now
+          formData.append(key, values[key]);
+        }
+      }
+
+      // Append files to FormData
+      if (values?.uploadimage?.fileList) {
+        values.uploadimage.fileList.forEach(file => {
+          if (file.originFileObj) {
+            formData.append('image', file.originFileObj);
+          }
+        });
+      }
+
+
+      const response = await axios.post(`${apiUrl}/addCar`, formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      });
+      reset()
       messageApi.info(response.data.message);
     } catch (error) {
       messageApi.info(error?.message);
